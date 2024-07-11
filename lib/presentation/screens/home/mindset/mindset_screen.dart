777ab472d6184d22bea20cfdb1271030562beed6
Alejandro Parrado <alejopadido13@gmail.com>
2024-07-11 16:44:32 -0500

    Added coming soon animated text on the image section #16

--- lib/presentation/screens/home/mindset/mindset_screen.dart
+++ lib/presentation/screens/home/mindset/mindset_screen.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:top/presentation/widgets/widgets.dart';
+import 'package:typewritertext/typewritertext.dart';
 
 class MindsetScreen extends StatelessWidget {
   const MindsetScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: TypeWriter.text(
+          '''
+<h1> Greetings, Achiever! :D </h1>
+
+<p> You've unlocked a secret area! :0 </p>
+
+<p> Here will be the "Image" hub </p>
+
+<p> I hope you're working hard! </p>
+
+<p> Cool stuff coming soon! </p>
+
+- Alejo ;)                                          
+        ''',
+          duration: const Duration(milliseconds: 50),
+          repeat: true,
+          overflow: TextOverflow.visible,
+          softWrap: false,
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ),
+    );
   }
 }
